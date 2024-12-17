4cad8ecb42f446cbc53c56bad021b28f9b19b56d
Pedro Lecaros Alfaro <pedro.lecaros@zentagroup.com>
2024-12-17 13:36:20 -0300

    exp

--- src/main/java/com/pruebaSermaluc/usuario/service/UserService.java
+++ src/main/java/com/pruebaSermaluc/usuario/service/UserService.java
@@ -4,7 +4,10 @@ import com.pruebaSermaluc.usuario.exception.CustomException;
 import com.pruebaSermaluc.usuario.model.User;
 import com.pruebaSermaluc.usuario.repository.UserRepository;
 import com.pruebaSermaluc.usuario.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
@@ -12,16 +15,34 @@ public class UserService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
+    @Value("${regex.email}")
+    private String emailRegex;
+
+    @Value("${regex.password}")
+    private String passwordRegex;
+
     public UserService(UserRepository userRepository, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
     }
 
     public User registerUser(User user) {
+        // Validar correo electrónico
+        if (!Pattern.matches(emailRegex, user.getEmail())) {
+            throw new CustomException("El correo no tiene un formato válido");
+        }
+
+        // Validar contraseña
+        if (!Pattern.matches(passwordRegex, user.getPassword())) {
+            throw new CustomException("La contraseña no cumple con los requisitos");
+        }
+
+        // Validar si el correo ya existe
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new CustomException("El correo ya está registrado");
         }
 
+        // Generar token y guardar el usuario
         user.setToken(jwtUtil.generateToken(user.getEmail()));
         return userRepository.save(user);
     }
